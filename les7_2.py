44e80b7ec17441937c30fe3ad9e7413898d4d720
gefest <gefest@me.com>
2020-11-03 22:04:22 +0300

    les 7

---
+++ les7_2.py
@@ -0,0 +1,27 @@
+class Temperature:
+    __KELVIN_CONST_C = 273.15
+    __slots__ = ('kelvin',)
+
+    def __init__(self):
+        self.kelvin = 0
+
+    @property
+    def celsius(self):
+        return self.kelvin - self.__KELVIN_CONST_C
+
+    @celsius.setter
+    def celsius(self, value):
+        self.kelvin = value + self.__KELVIN_CONST_C
+
+    @property
+    def fahrenheit(self):
+        return self.kelvin * 1.8 - 459.67
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.kelvin = (value + 459.67) / 1.8
+
+
+if __name__ == '__main__':
+    termo = Temperature()
+    print(1)
